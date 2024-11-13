9dd0f08c8e9ad4a9b6dcf004f885729bafd78359
Austin He <ahe8@my.bcit.ca>
2024-11-13 12:14:52 -0800

    add broken health path

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('1.2') // change this string to ensure a new version deployed
+  res.send('1.3') // change this string to ensure a new version deployed
 })
 
 
